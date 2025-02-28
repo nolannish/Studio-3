aa030f22c3559133f2e61307c43839eb56a31744
nolannish <nolannish@gmail.com>
2025-02-28 13:41:54 -0800

    Added coin asset and coin rotation

---
+++ Assets/Scripts/CoinRotation.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CoinRotation : MonoBehaviour
+{
+    public Vector3 rotation;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+}

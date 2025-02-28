ef2de6bf2260663968ad2b21fd688bb9cb4367d5
nolannish <nolannish@gmail.com>
2025-02-28 13:07:25 -0800

    Jumping is now working

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,19 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed;
 
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public KeyCode jumpKey = KeyCode.Space;
+
     public Transform orientation;
 
     float horizontalInput;
@@ -19,12 +32,21 @@ public class Player : MonoBehaviour
     {
        rb = GetComponent<Rigidbody>();
        rb.freezeRotation = true;
+       readyToJump = true;
 
     }
 
     void Update()
     {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); 
+
         MyInput();
+
+        if(grounded){
+            rb.linearDamping = groundDrag;
+        } else {
+            rb.linearDamping = 0;
+        }
     }
 
     private void FixedUpdate()
@@ -36,13 +58,33 @@ public class Player : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey) && readyToJump && grounded){
+            Debug.Log("space");
+            readyToJump = false;
+            JumpPlayer();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if(grounded){
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
 
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    private void JumpPlayer(){
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump(){
+        readyToJump = true; 
     }
 
 }
